c8c9760a4f73f1761008491ac5dba4441e2b1d41
Caleb Hornbeck <inventor96@gmail.com>
2025-09-15 13:22:56 -0600

    Enhance pagination icons with line-height utilities for better alignment

--- app/resources/views/Components/Pager.vue
+++ app/resources/views/Components/Pager.vue
@@ -33,12 +33,12 @@ const lastRecordNumber = computed(() => {
 			<ul class="pagination">
 				<li class="page-item">
 					<Link class="page-link" href="?page=1" :class="{ disabled: !props.pagination.previous }" aria-label="First">
-						<span aria-hidden="true"><i class="bi bi-chevron-double-left"></i>&nbsp;</span>
+						<span aria-hidden="true"><i class="bi lh-base bi-chevron-double-left"></i></span>
 					</Link>
 				</li>
 				<li class="page-item">
 					<Link class="page-link" :href="props.pagination.previous ?? '#'" :class="{ disabled: !props.pagination.previous }" aria-label="Previous">
-						<span aria-hidden="true"><i class="bi bi-chevron-left"></i>&nbsp;</span>
+						<span aria-hidden="true"><i class="bi lh-base bi-chevron-left"></i></span>
 					</Link>
 				</li>
 				<li class="page-item" :class="{ active: page === props.pagination.current_page }" v-for="page in Array.from({ length: props.pagination.number_of_pages }, (_, i) => i + 1)" :key="page">
@@ -48,12 +48,12 @@ const lastRecordNumber = computed(() => {
 				</li>
 				<li class="page-item">
 					<Link class="page-link" :href="props.pagination.next ?? '#'" :class="{ disabled: !props.pagination.next }" aria-label="Next">
-						<span aria-hidden="true">&nbsp;<i class="bi bi-chevron-right"></i></span>
+						<span aria-hidden="true"><i class="bi lh-base bi-chevron-right"></i></span>
 					</Link>
 				</li>
 				<li class="page-item">
 					<Link class="page-link" :href="`?page=${props.pagination.number_of_pages}`" :class="{ disabled: !props.pagination.next }" aria-label="Last">
-						<span aria-hidden="true">&nbsp;<i class="bi bi-chevron-double-right"></i></span>
+						<span aria-hidden="true"><i class="bi lh-base bi-chevron-double-right"></i></span>
 					</Link>
 				</li>
 			</ul>
