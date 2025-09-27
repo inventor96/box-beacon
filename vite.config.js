365ab87451eb6648f21543187ab6d3a361054e29
Caleb Hornbeck <inventor96@gmail.com>
2025-09-27 01:56:47 -0600

    Refactored docker stuff to support separate dev and prod configs; used .env file for port customization

--- vite.config.js
+++ vite.config.js
@@ -1,46 +1,54 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'app/resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    const vitePort = env.VITE_PORT ? Number(env.VITE_PORT) : 5173;
+
+    return {
+        plugins: [
+            laravel({
+                input: 'app/resources/js/app.js',
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+            vueDevTools({
+                appendTo: 'app/resources/js/app.js',
+            }),
+        ],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'app/resources/views'),
+            },
+        },
+        build: {
+            outDir: 'public/build',
+            assetsDir: 'assets',
+        },
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    silenceDeprecations: [
+                        'import',
+                        'mixed-decls',
+                        'color-functions',
+                        'global-builtin',
+                    ],
                 },
             },
-        }),
-        vueDevTools({
-            appendTo: 'app/resources/js/app.js',
-        }),
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'app/resources/views'),
         },
-    },
-    build: {
-        outDir: 'public/build',
-        assetsDir: 'assets',
-    },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                silenceDeprecations: [
-                    'import',
-                    'mixed-decls',
-                    'color-functions',
-                    'global-builtin',
-                ],
-            },
+        server: {
+            port: vitePort,
         },
-    },
+    };
 });
