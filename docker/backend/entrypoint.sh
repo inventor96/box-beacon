abe6640d1a7a47aba613506d150e3d02f6367b69
Caleb Hornbeck <inventor96@gmail.com>
2025-09-28 21:24:27 -0600

    Made backend user idempotent

--- docker/backend/entrypoint.sh
+++ docker/backend/entrypoint.sh
@@ -18,8 +18,8 @@ fi
 
 # If the command is in the list, we want to run it as the host-equivalent user
 if [ "$1" = "composer" ] || [ "$1" = "php" ]; then
-  echo "Running command as devuser: $*"
-  exec gosu devuser "$@"
+  echo "Running command as $UID:$GID: $*"
+  exec gosu "$UID:$GID" "$@"
 else
   # Hand off to whatever command was passed
   exec "$@"
