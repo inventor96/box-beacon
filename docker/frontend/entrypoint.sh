ec455ae9b7fd566b36b67e0e583bdd0b3cd6a800
Caleb Hornbeck <inventor96@gmail.com>
2025-09-28 20:36:32 -0600

    Update entrypoint.sh to run npm commands as host-equivalent user except for 'npm run dev'

--- docker/frontend/entrypoint.sh
+++ docker/frontend/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# If the command is in the list, we want to run it as the host-equivalent user
-if [ "$1" = "npm" ] && [ "$2" = "install" ]; then
+# If the command is `npm`, but NOT `run dev`, we want to run it as the host-equivalent user
+if [ "$1" = "npm" ] && ! ([ "$2" = "run" ] && [ "$3" = "dev" ]); then
   echo "Running command as $UID:$GID: $*"
   exec gosu "$UID:$GID" "$@"
 else
