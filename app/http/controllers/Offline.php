e1f49423c2b75fa5b9d2925c52b050de2cd6b90d
Caleb Hornbeck <inventor96@gmail.com>
2025-11-25 02:09:32 -0700

    Disallow guests from accessing offline routes

--- app/http/controllers/Offline.php
+++ app/http/controllers/Offline.php
@@ -5,6 +5,11 @@ use app\modules\offline\OfflineRoutes;
 
 class Offline extends ControllerBase {
 	public function index(OfflineRoutes $offline) {
+		// disallow guests
+		if (!$this->getUser()) {
+			return $this->jsonResponse([], 403);
+		}
+
 		$routes = $offline->generateRoutes();
 		return $this->jsonResponse($routes);
 	}
