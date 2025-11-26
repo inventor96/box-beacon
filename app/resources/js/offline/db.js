975d2ac65da49a4e97d737f42433951bcb140da8
Caleb Hornbeck <inventor96@gmail.com>
2025-11-26 03:08:42 -0700

    Fix db export; ignore build files

--- app/resources/js/offline/db.js
+++ app/resources/js/offline/db.js
@@ -1,10 +1,8 @@
 import Dexie from 'dexie';
 
-const db = new Dexie('InertiaOfflineDB');
+export const db = new Dexie('InertiaOfflineDB');
 
 db.version(1).stores({
 	pages: '&url, savedAt, expiresAt',      // url is primary key
 	routeMeta: '&url, ttl'                  // routes provided by backend
 });
-
-export default db;
