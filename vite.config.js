28f36817c15e2c490f180eb3c743da0f15383a56
Caleb Hornbeck <inventor96@gmail.com>
2026-03-15 20:02:38 -0600

    Added images

--- vite.config.js
+++ vite.config.js
@@ -13,33 +13,33 @@ export default defineConfig(({ mode }) => {
 
 	const manifestIcons = [
 		{
-			src: '/pwa-64x64.png',
+			src: '/favicon-64x64.png',
 			sizes: '64x64',
 			type: 'image/png'
 		},
 		{
-			src: '/pwa-192x192.png',
+			src: '/android-chrome-192x192.png',
 			sizes: '192x192',
 			type: 'image/png'
 		},
 		{
-			src: '/pwa-512x512.png',
+			src: '/android-chrome-512x512.png',
 			sizes: '512x512',
 			type: 'image/png',
 			purpose: 'any'
 		},
-		{
+		/* {
 			src: '/maskable-icon-512x512.png',
 			sizes: '512x512',
 			type: 'image/png',
 			purpose: 'maskable'
-		}
+		} */
 	];
 
 	const publicIcons = [
 		{ src: '/favicon.ico' },
-		{ src: '/favicon.svg' },
-		{ src: '/apple-touch-icon-180x180.png' }
+		//{ src: '/favicon.svg' },
+		{ src: '/apple-touch-icon.png' }
 	];
 
 	const additionalImages = [];
